60ef0e78bbcb42b71e1a84f2be45b50df636018d
roomanalam <92167231+roomanalam@users.noreply.github.com>
2021-12-27 16:55:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash,abort
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 import datetime
 from flask_sqlalchemy import SQLAlchemy
